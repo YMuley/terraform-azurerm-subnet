4a6ae0b9d162d06a978cdc8ff6076ee67c08eb5d
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-14 00:28:46 +0530

    new change

--- output.tf
+++ output.tf
@@ -3,6 +3,6 @@ output "vnet_subnet_output" {
   description = "Outputs all vnet_subnet objects"
 }
 
-output "remote_state" {
-  value = data.terraform_remote_state.primary-state.outputs
-}
+# output "remote_state" {
+#   value = data.terraform_remote_state.primary-state.outputs
+# }
