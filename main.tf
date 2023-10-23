55ed9c33ea4af760c45059ecfc44c434f396da1f
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-10-23 20:15:09 +0530

    code updatted

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_subnet" "subnet" {
  resource_group_name            = var.resource_group_output[each.value.resource_group_name].name #each.value.resource_group_name   #data.terraform_remote_state.primary-state.outputs.resource_group_output[each.value.resource_group_name].name
  virtual_network_name           = var.virtual_network_output[each.value.virtual_network_name].name #each.value.virtual_network_name    #data.terraform_remote_state.primary-state.outputs.virtual_network_output[each.value.virtual_network_name].name 
  address_prefixes               = each.value.address_prefixes
- service_endpoints              = each.value.service_endpoints
+ service_endpoints              =  each.value.service_endpoints
  service_endpoint_policy_ids    = length(each.value.service_endpoint_policy_ids) == 0 ? null : flatten([ for service_endpoint_policy in var.service_endpoint_policy_output : service_endpoint_policy.id  if contains(each.value.service_endpoint_policy_ids,service_endpoint_policy.name) == true ])
  private_endpoint_network_policies_enabled      = each.value.private_endpoint_network_polices_enabled
  private_link_service_network_policies_enabled  = each.value.private_link_service_network_policies_enabled
@@ -15,9 +15,9 @@ resource "azurerm_subnet" "subnet" {
         content{
             name = delegation.value.name
                 dynamic "service_delegation"{
-                    for_each = delegation.value.service_delegation
+                    for_each = delegation.value.service_delegation != [] ? [1] : []
                         content{
-                            name    = lookup(service_delegation.value,each.key,null) == null ? null : lookup(service_delegation.value,each.key,null)
+                            name    = service_delegation.value.name
                             actions = service_delegation.value.actions
                         }
                 }
